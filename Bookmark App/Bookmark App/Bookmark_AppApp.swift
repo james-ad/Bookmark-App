c702ccb2b96ddd2a7544ce7912dc38e3933553ba
James Dunn <jamesdu@zillowgroup.com>
2021-12-17 21:24:30 -0800

    Set up initial SwiftUI files

---
+++ Bookmark App/Bookmark App/Bookmark_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Bookmark_AppApp.swift
+//  Bookmark App
+//
+//  Created by James Dunn on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Bookmark_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
