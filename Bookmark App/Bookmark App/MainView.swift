db387250e1cae3a55f90fe7568b724cbf88d64c4
James Dunn <jamesdu@zillowgroup.com>
2021-12-30 19:07:15 -0800

    Set up first page of app

---
+++ Bookmark App/Bookmark App/MainView.swift
@@ -0,0 +1,60 @@
+//
+//  MainView.swift
+//  Bookmark App
+//
+//  Created by James Dunn on 12/29/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            VStack(alignment: .center, spacing: 50, content: {
+                Spacer()
+                Text("Bookmark")
+                    .font(.system(size: 75))
+                Spacer()
+                Spacer()
+                Spacer()
+                CaptureButton()
+                Spacer()
+            })
+                .tabItem {
+                    VStack {
+                    Image(systemName: "camera")
+                    Text("Capture Quote")
+                    }
+                }
+        }
+    }
+    
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
+struct CaptureButton: View {
+    var body: some View {
+        Group {
+            Button(action: handleTap) {
+                Text("Capture Photo")
+                    .font(.largeTitle)
+            }
+            .frame(maxWidth: 300, maxHeight: 300)
+        }
+        .background(.blue)
+        .foregroundColor(.white)
+        .cornerRadius(200)
+        .onTapGesture {
+            handleTap()
+        }
+    }
+    
+    private func handleTap() {
+        print("Button Tapped!!")
+    }
+}
