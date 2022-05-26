45a395039c2b4cef17067e43becbdf9351cb7b3b
James Dunn <jamesdunn@Jamess-Mac-Studio.local>
2022-05-26 12:05:15 -0700

    Import AlertToast before integration

--- Bookmark App/Bookmark App/Views/MainView.swift
+++ Bookmark App/Bookmark App/Views/MainView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AlertToast
 
 protocol QuoteCaptureDelegate: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     func confirmPhotoPermissions() -> Bool
