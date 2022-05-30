5513569adda2cd8132ce606330a23eed6476df55
James Dunn <jamesdu@zillowgroup.com>
2022-05-30 11:22:31 -0700

    Integrate Firebase project with app and create StoreManager

--- Bookmark App/Bookmark App/Bookmark_App.swift
+++ Bookmark App/Bookmark App/Bookmark_App.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Bookmark_App: App {
+    // Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     let persistenceController = PersistenceController.shared
     @StateObject private var store = testStore
     @StateObject var capturedQuote = CapturedQuote()
@@ -16,8 +29,6 @@ struct Bookmark_App: App {
     
     var body: some Scene {
         WindowGroup {
-            //            ContentView()
-            //            .environment(\.managedObjectContext, persistenceController.container.viewContext)
             TabView {
                 MainView(delegate: QuoteCaptureViewController())
                     .tabItem {
---
+++ Bookmark App/Bookmark App/Storage/StorageManager.swift
@@ -0,0 +1,13 @@
+//
+//  StorageManager.swift
+//  Bookmark App
+//
+//  Created by James Dunn on 5/30/22.
+//
+
+import SwiftUI
+import FirebaseStorage
+
+class StorageManager: ObservableObject {
+    let storage = Storage.storage()
+}
